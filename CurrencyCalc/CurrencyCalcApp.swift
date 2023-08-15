79385b0b04a9fb269c55ac5b5396178a4a746ac1
Midori <fujiakira1116@gmail.com>
2023-08-15 16:18:36 +0900

    Initial Commit

---
+++ CurrencyCalc/CurrencyCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyCalcApp.swift
+//  CurrencyCalc
+//
+//  Created by Akira on 2023/08/15.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
